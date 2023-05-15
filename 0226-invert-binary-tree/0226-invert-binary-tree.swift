b861732425b3794cd006513ac93fe4095ba87640
TaeHyung Kim (Ted Kim) <104834390+Taerogrammer@users.noreply.github.com>
2023-05-16 01:05:00 +0900

    Time: 3 ms (80.13%), Space: 14.3 MB (42.09%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.swift
+++ 0226-invert-binary-tree/0226-invert-binary-tree.swift
@@ -14,10 +14,8 @@
  * }
  */
 
-
 // 튜플 할당 
 // 여러 값을 한 번에 할당하는 방법으로 소괄호를 사용하여 값을 묶어 튜플을 생성하고, 변수나 상수에 할당할 수 있음
-
 
 class Solution {
     func invertTree(_ root: TreeNode?) -> TreeNode? {
@@ -26,7 +24,7 @@ class Solution {
             return nil
         }
         
-        //원래 노드의 오른쪽 값을 root.left에, 원래 노으듸 왼쪽 값을 root.right에 대입해줌
+        //원래 노드의 오른쪽 값을 root.left에, 원래 노드의 왼쪽 값을 root.right에 대입해줌
         (root.left, root.right) = (invertTree(root.right), invertTree(root.left))
         
         return root
