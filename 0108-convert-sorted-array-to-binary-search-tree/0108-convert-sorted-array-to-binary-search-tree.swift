9d1018e6a22d02aebf54e9e323108ea61f6ee9f8
TaeHyung Kim (Ted Kim) <104834390+Taerogrammer@users.noreply.github.com>
2023-05-20 16:25:31 +0900

    Time: 16 ms (75.91%), Space: 14.8 MB (73.72%) - LeetHub

---
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.swift
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+
+/**
+1. 노드에서 중간값을 구한다.
+2. 중간값을 기준으로 TreeNode를 생성한다.
+3. 중간값보다 작으면 left, 크면 right에 값을 넣는다.
+*/
+class Solution {
+    func sortedArrayToBST(_ nums: [Int]) -> TreeNode? {
+        return convertToBST(nums, 0, nums.count - 1)
+    }
+    
+    func convertToBST(_ nums: [Int], _ left: Int, _ right: Int) -> TreeNode? {
+        //nums가 존재하지 않을 때
+        if left > right {
+            return nil
+        }
+        // 
+        let mid = (left + right) / 2
+        let node = TreeNode(nums[mid])
+        
+        node.left = convertToBST(nums, left, mid - 1)
+        node.right = convertToBST(nums, mid + 1, right)
+        
+        return node
+    }
+}
