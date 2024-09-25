f9414a47affc3f61e945d03e3162eda6ac7997fe
TaeHyung Kim (Ted Kim) <104834390+Taerogrammer@users.noreply.github.com>
2024-09-26 00:31:21 +0900

    Update 0067-add-binary.py

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,3 +1,16 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
+        aList = list(map(int, a))
+        bList = list(map(int, b))
+        aList.reverse()
+        bList.reverse()
+        (aValue, bValue) = (0, 0)
+        for i in range(len(aList)):
+            aValue += aList[i] * pow(2, i)
+        for i in range(len(bList)):
+            bValue += bList[i] * pow(2, i)
+            
+        return bin(aValue+bValue)[2:]
+    # 다른 풀이 (format)
+    def addBinary(self, a: str, b: str) -> str:
         return format(int(a, 2) + int(b, 2), 'b')
