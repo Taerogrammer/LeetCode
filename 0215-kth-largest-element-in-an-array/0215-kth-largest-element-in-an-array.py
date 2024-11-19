d5541749933a4d5d46bf73202758d3c35f8c418b
TaeHyung Kim (Ted Kim) <104834390+Taerogrammer@users.noreply.github.com>
2024-11-19 11:14:14 +0900

    Update 0215-kth-largest-element-in-an-array.py

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -9,3 +9,17 @@ class Solution:
             heapq.heappop(nums)
         
         return heapq.heappop(nums)
+
+
+        ## 처음 풀이
+        heap = list()
+        
+        # 최대 힙으로 삽입
+        for num in nums:
+            heapq.heappush(heap, -num)
+        # k-1번째까지 pop
+        for i in range(k-1):
+            heapq.heappop(heap)
+        
+        # k번째 pop
+        return -heapq.heappop(heap)
